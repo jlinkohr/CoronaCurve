a886b62c050e26f7280ec8f64a2ed132ce770e04
jlinkohr <Jochen.Linkohr@gmx.de>
2020-03-20 00:12:55 +0100

    data and code checkin

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Plot some Corona measures (because I did not find the KPIs of my personal intere
 
 I just wanted to know whether the number of corona new infected people goes up faster or slower tha the last day. found it on now website,so I decided to have some coding fun ;)
 
+Data is taken from : https://interaktiv.morgenpost.de/corona-virus-karte-infektionen-deutschland-weltweit/
+
 jochen.linkohr@gmx.de
--- report.py
+++ report.py
@@ -1,19 +1,94 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-dataFile = "coronaData.csv"
+dataFile = "coronaData_GER.csv"
 
 # read the data
-data = pd.read_csv(dataFile)
+total = pd.read_csv(dataFile)
 
-total=data[['Date', 'Total']]
+#total=data[['Date', 'Total']]
 
 print (f"Totals:\n{total}")
-plt.figure()
+
+
+
+#plt.show()
+#print(data)
+
+# calculate the difference of totals
+
+i = 0
+AktDiff = pd.DataFrame(columns=['Diff', 'Percent', 'DiffDiff'])
+lastDiff = 0
+for x in range(len(total)):
+
+    aktval=total.iloc[i]['Total']
+    if (i != 0):
+        lastval=total.iloc[i-1]['Total']
+    else:
+       lastval = 0
+    i=i+1
+
+    # calculate difference between last value and actual value
+    diff=aktval-lastval
+
+    # calculate percentage of change in relation to last value
+    if (lastval != 0):
+        percent=diff/lastval*100
+    else:
+        #if first value, set to zero
+        percent=0
+
+    # calculate difference of difference (2nd diff)
+    DiffDiff = diff-lastDiff
+    lastDiff = diff
+
+    AktDiff = AktDiff.append({'Diff' : diff, 'Percent': percent, 'DiffDiff' : DiffDiff}, ignore_index=True)
+    #print(f"line {x}: {diff}\nAktDiff: \n{AktDiff}\n")
+
+print (AktDiff)
+outValue = pd.concat([total , AktDiff], axis=1)
+print (outValue)
+
+SMALL_SIZE = 6
+MEDIUM_SIZE = 8
+BIGGER_SIZE = 8
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+fig, axes = plt.subplots(3,1)
+axes[0].xaxis.set_visible(False) 
+axes[1].xaxis.set_visible(False)
+
+
+outValue.plot(x='Date', y='Total', ax=axes[0], title='data taken from Berliner Morgenpost - ' + dataFile + '\n\nAnzahl Infizierte')
+
+axes[0].get_legend().remove()
+
+outValue.plot(x='Date', y='Curated', color='green', ax=axes[1], title='Anzahl wieder gesund')
+
+axes[1].get_legend().remove()
+
+outValue.plot(x='Date', y='Deaths', color='red', ax=axes[2], title='Anzahl Tote')
+axes[2].get_legend().remove()
+
+plt.tight_layout()
+
+# ------------ second chart -------------
+fig, axes = plt.subplots(3,1)
+axes[0].xaxis.set_visible(False) 
+axes[1].xaxis.set_visible(False)
+
+outValue.plot.bar(x='Date', y='Percent', color='red', ax = axes[0], title='data taken from Berliner Morgenpost - ' + dataFile + '\n\nNeuinfektionen in Prozent zur Gesamtzahl der Infizierten')
+outValue.plot.bar(x='Date', y='Diff', color='blue', ax = axes[1], title='Unterschied zum Vortag (absolut)')
+outValue.plot.bar(x='Date', y='DiffDiff', color='blue', ax = axes[2], title='Unterschiedsänderung zum Vortag (absolut)')
+plt.tight_layout()
+
 plt.show()
-
-total.plot.bar()
-
-print(data)
 
 p = input()
